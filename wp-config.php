4b8ff4cf4876ea6eb38a35b93dfa7f97ba7ba3c0
M Abu Bakar Khan <mabubakarkhan@users.noreply.github.com>
2023-03-31 21:25:27 +0500

    Change DB details

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Muzic' );
+define( 'DB_NAME', 'wp_music' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
